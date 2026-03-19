211fa54c62f3fa42b09eee1b14fa294a98280a2a
ktk3376 <ktk3376@gmail.com>
2026-03-19 10:50:36 +0900

    hooks, localStorage 적용

--- react_todo_mession1/src/App.jsx
+++ react_todo_mession1/src/App.jsx
@@ -1,44 +1,10 @@
-import { useState } from 'react'
 import TodoForm from './component/TodoForm.jsx'
 import TodoList from './component/TodoList.jsx'
 import TodoStats from './component/TodoState.jsx'
-
-const initialTodos = [
-    { id: 1, todo: ' 공부하기', completed: false },
-    { id: 2, todo: '컴포넌트 분리 연습', completed: true },
-    { id: 3, todo: 'TodoList 만들기', completed: false },
-]
+import useTodo from './hooks/useTodo.js'
 
 function App() {
-    const [todos, setTodos] = useState(initialTodos)
-    const [nextId, setNextId] = useState(4)
-
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-        const form = e.target
-
-        if (!form.todo.value.trim()) {
-            // input이 비어있거나 공백만 있을 때 실행
-            alert('할일을 입력해주세요!')
-            return
-        }
-
-        setTodos([...todos, { id: nextId, todo: form.todo.value, completed: false }])
-        setNextId(nextId + 1)
-        form.reset()
-    }
-
-    const removeTodo = (seletedId) => {
-        const filterTodos = todos.filter((todo) => todo.id !== seletedId)
-        setTodos(filterTodos)
-    }
-
-    const toggleTodo = (seletedId) => {
-        const updateTodos = todos.map((todo) =>
-            todo.id === seletedId ? { ...todo, completed: !todo.completed } : todo,
-        )
-        setTodos(updateTodos)
-    }
+    const { todos, handleOnSubmit, removeTodo, toggleTodo } = useTodo()
 
     return (
         <>
--- react_todo_mession1/src/component/TodoState.jsx
+++ react_todo_mession1/src/component/TodoState.jsx
@@ -1,7 +1,7 @@
 function TodoStats({ todos }) {
     const total = todos.length
-    const completed = todos.filter((todo) => todo.completed).length
-    const remaining = todos.filter((todo) => !todo.completed).length
+    const completed = todos.filter((todo) => todo.completed).length // 완료된 할일 개수
+    const remaining = todos.filter((todo) => !todo.completed).length // 남은 할일 개수
 
     return (
         <>
@@ -9,6 +9,7 @@ function TodoStats({ todos }) {
                 전체 {total}개 / 완료 {completed}개 / 남은 할일 {remaining}개
             </p>
             {remaining === 0 && total > 0 && <p> 모든 할일을 완수했습니다!</p>}
+            {/* 남은 할일 0개, 전체 할일 0개 이상일 시 메세지 출력 */}
         </>
     )
 }
